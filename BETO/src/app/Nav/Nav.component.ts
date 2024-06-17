c63ea02a28ab1c9a7f0a137627904da9a5d5a525
Alberto Williams <68757297+alberto1996-23@users.noreply.github.com>
2024-06-17 12:17:18 -0500

    Delete BETO directory

--- BETO/src/app/Nav/Nav.component.ts
+++
@@ -1,8 +0,0 @@
-import { Component } from '@angular/core';
-
-@Component({
-  selector: 'app-Nav',
-  templateUrl: './Nav.component.html',
-  styleUrl: './Nav.component.css'
-})
-export class NavComponent {}
--- BETO/src/app/interests/interests.component.ts
+++
@@ -1,8 +0,0 @@
-import { Component } from '@angular/core';
-
-@Component({
-  selector: 'app-interests',
-  templateUrl: './interests.component.html',
-  styleUrl: './interests.component.css'
-})
-export class InterestsComponent {}
