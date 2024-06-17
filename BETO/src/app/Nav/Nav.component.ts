fcdc6cf3615317e16ba280525b4160f82d46e745
alberto1996-23 <albertowilliams16@gmail.com>
2024-06-17 00:07:31 -0500

    Angular modifications

---
+++ BETO/src/app/Nav/Nav.component.ts
@@ -0,0 +1,8 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-Nav',
+  templateUrl: './Nav.component.html',
+  styleUrl: './Nav.component.css'
+})
+export class NavComponent {}
---
+++ BETO/src/app/interests/interests.component.ts
@@ -0,0 +1,8 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-interests',
+  templateUrl: './interests.component.html',
+  styleUrl: './interests.component.css'
+})
+export class InterestsComponent {}
